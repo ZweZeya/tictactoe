a18f7ed70b04f333945a02a709138403f3d83e79
zwe <zwezeya02@gmail.com>
2022-12-29 21:44:45 +0800

    Initial Commit

---
+++ tictactoe/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  tictactoe
+//
+//  Created by Zwe Zeya on 29/12/22.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
